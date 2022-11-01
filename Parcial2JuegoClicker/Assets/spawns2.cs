4694926ddc1a09005e4586d343dd32ab7d3f3df0
urielReyesMartinez <urielreyes0728m@gmail.com>
2022-11-01 14:24:32 -0600

    PARCIAL 2 JUEGO
    creacion de spawns en objetivos con vector 2

---
+++ Parcial2JuegoClicker/Assets/spawns2.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spaens : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector2 randomSpawnPosition = new Vector2(Random.Range(-2, 5), Random.Range(-2, 5));
+            Instantiate(enemyPrefab, randomSpawnPosition, Quaternion.identity);
+        }
+    }
+}
