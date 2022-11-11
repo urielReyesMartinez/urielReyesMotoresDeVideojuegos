a82aad9eb5edd5ad01869623785931c92c9dea44
urielReyesMartinez <urielreyes0728m@gmail.com>
2022-11-10 18:06:42 -0600

    parcial juego
    creacion de nuevo spawners pero aun no se destruye fata codificar

---
+++ Parcial2JuegoClicker/Assets/spaner.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spaner : MonoBehaviour
+{
+    public Transform enemigos;
+    public Transform enemigos2;
+    public Transform enemigos3;
+    public Transform enemigos4;
+    public Transform enemigos5;
+
+    public GameObject[] enemyPrefab;
+    public float tiemspawner=1;
+    public float repeatspawner = 1;
+   // public float tiempdristr = 1;
+
+    // inicializar el spawner
+    void Start()
+    {
+        InvokeRepeating("spawnerenemies",tiemspawner,repeatspawner);
+        //InvokeRepeating("destrucion", tiempdristr, tiemspawner);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //dretuir game object
+    //private void Destroy(GameObject[] enemyPrefab)
+    //{
+       // throw new System.NotImplementedException();
+    //}
+
+    //posicionar donde apareceran los fantasmas en prefab
+    public void spawnerenemies()
+    {
+        Vector3 spawposition = new Vector3(0, 0,0);
+        spawposition = new Vector3(Random.Range(enemigos2.position.x, enemigos4.position.x), Random.Range(enemigos.position.y, enemigos5.position.y),0);
+        GameObject enemy = Instantiate(enemyPrefab[Random.Range(0,enemyPrefab.Length)],spawposition,gameObject.transform.rotation);
+        
+    }
+    
+}
