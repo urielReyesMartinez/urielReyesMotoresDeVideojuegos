8a31993a31558cd2c5218c750c150d610aa9e16e
urielReyesMartinez <urielreyes0728m@gmail.com>
2022-11-23 18:50:42 -0600

    parcial audios
    creacion de scripts de audios en soudtracks y efectos

---
+++ Parcial3Uteca/Assets/AudioManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum audioClips_soundtrack//referencias de sonidos de las canciones
+{
+    none,
+    song_1,
+    song_2,
+}
+
+public enum sudioclips_sfx//referenias de efectos de sonidos como objetos
+{
+    none,
+    walk,
+    auto,
+    punch,
+    bullet,
+}
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    
+    public AudioSource audioSource;// brackground music / soundtrack / songs
+    public AudioSource audioSourceEffectref;//vfx / ui
+    public AudioClip[] audioClipsSoundtrack;
+    public AudioClip[] audioClipsSfx;
+    public float soundtrackVolume=(3f);
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+    public void setAudioclipSoundtrack(audioClips_soundtrack audioClips_Soundtrack)
+    {
+        audioSource.Stop();//detener el audio anterior existente
+        audioSource.PlayDelayed(0.3f);//tiempo de detenido para reproducir siguiente
+        //reproduccion de la cancion con un volumen dado
+        audioSource.PlayOneShot(audioClipsSoundtrack[(int)audioClips_Soundtrack],soundtrackVolume);
+    }
+}
