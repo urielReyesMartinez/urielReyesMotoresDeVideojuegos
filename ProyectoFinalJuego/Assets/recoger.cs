e9287f0556268e6e30cf21e5059006332f15cd77
urielReyesMartinez <urielreyes0728m@gmail.com>
2022-12-06 19:19:59 -0600

    parcial juego
    ineventario

---
+++ ProyectoFinalJuego/Assets/recoger.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class recoger : MonoBehaviour
+{
+    private Inventory inventory;
+    public GameObject itembutt;
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Playe").GetComponent<Inventory>();
+
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            for (int i = 0; i <inventory.slots.Length; i++)
+            {
+               // if (inventory.isFull[i]==false)
+              //  {
+              //      Instantiate(itembutt, inventory.slots[i].transform, false);
+              //      inventory.isfull[i] = true;
+              //      Destroy(gameObject);
+               //     break;
+               // }
+               
+            }
+        }
+    }
+   
+
+	
+
+}
