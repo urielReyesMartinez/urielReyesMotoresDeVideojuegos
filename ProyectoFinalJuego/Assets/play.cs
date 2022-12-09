af619edcfe13fff6b4be2d99fee37e47ed8d05a7
urielReyesMartinez <urielreyes0728m@gmail.com>
2022-12-08 18:01:10 -0600

    parcial juego
    creacion de multiplayer de dos jugadores pantalla dividida

--- ProyectoFinalJuego/Assets/play.cs
+++ ProyectoFinalJuego/Assets/play.cs
@@ -2,32 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movimiento : MonoBehaviour
+public class play : MonoBehaviour
 {
     public float velocidad = 3f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A)) //movimiento horizontal //izquierda
+        if (Input.GetKey(KeyCode.LeftArrow)) //movimiento horizontal
         {
             transform.position += Vector3.left * velocidad * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.D)) //derecha
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += Vector3.right * velocidad * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.W))//movimiento vertical //arriba
+        if (Input.GetKey(KeyCode.UpArrow))//movimiento vertical
         {
             transform.position += Vector3.up * velocidad * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.S)) //abajo
+        if (Input.GetKey(KeyCode.DownArrow))
         {
             transform.position += Vector3.down * velocidad * Time.deltaTime;
         }
